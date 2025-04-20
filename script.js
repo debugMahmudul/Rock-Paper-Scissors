6679e37907bd1f4bb0f2367ed2132629ec252cd8
debugMahmudul <mahmudul.hamim10@gmail.com>
2025-04-20 16:49:39 -0300

    Add function getHumanChoice to get the user input Add counter to keep track of plyaers scores

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+humanScore = 0;
+computerScore = 0;
+
+
 function getComputerChoice() {
     let randomNumber = Math.random();
     if (randomNumber > 0 && randomNumber <= 0.33) {
@@ -9,3 +13,10 @@ function getComputerChoice() {
     }
 }
 console.log(getComputerChoice());
+
+function getHumanChoice() {
+    let userChoice = prompt("Choose one from Rock,Paper or Scissors: ");
+    return userChoice;
+}
+console.log(getHumanChoice());
+
