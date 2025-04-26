ab81757cbeb4952141666b59d80c20f6875afb16
debugMahmudul <mahmudul.hamim10@gmail.com>
2025-04-25 23:55:55 -0300

    Add function to play one round

--- script.js
+++ script.js
@@ -1,22 +1,62 @@
-humanScore = 0;
-computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
 
 function getComputerChoice() {
     let randomNumber = Math.random();
     if (randomNumber > 0 && randomNumber <= 0.33) {
-        return "Rock";
+        return "rock";
     } else if (randomNumber > 0.33 && randomNumber <= 0.66) {
-        return "Paper";
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
     }
 }
-console.log(getComputerChoice());
 
 function getHumanChoice() {
     let userChoice = prompt("Choose one from Rock,Paper or Scissors: ");
     return userChoice;
 }
-console.log(getHumanChoice());
+
+// Play one round
+function playRound(humanChoice, computerChoice) {
+    // your code here!
+    humanChoice === humanChoice.toLowerCase();
+    //If computer choice and human choice match, it's a tie!
+    if (humanChoice === computerChoice) {
+        return "It's a tie!";
+    }
+    //Human win cases
+    if (humanChoice === "rock" && computerChoice === "scissors") {
+        humanScore++;
+        return "You win! Rock beats Scissors";
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        humanScore++;
+        return "You win! Paper beats Rock";
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        humanScore++;
+        return "You win! Scissors beats Paper";
+    }
+    // Computer Win cases
+    else if (humanChoice === "rock" && computerChoice === "paper") {
+        computerScore++;
+        return "You lose! Paper beats Rock";
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        computerScore++;
+        return "You lose! Scissors beat Paper";
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        computerScore++;
+        return "You lose! Rock beats Scissors";
+    } else {
+        return "Invalid choice.";
+    }
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
+
+
 
